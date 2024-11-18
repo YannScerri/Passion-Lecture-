45f00fc3e110e63dd9240822b2d690931ee56224
DanyCarn <pc23wsc@eduvaud.ch>
2024-11-18 12:01:34 +0100

    creation des dossiers d'organisation et création du fichier du header

---
+++ src/php/header.php
@@ -0,0 +1,20 @@
+<div class="container-header">
+    <div class="titre-header">
+        <h1>Passion Lecture</h1>
+    </div>
+    <div class="login-container">
+        <form action="connexion.php" method="post">
+            <label for="user"> </label>
+            <input type="text" name="user" id="user" placeholder="Login">
+            <label for="password"> </label>
+            <input type="password" name="password" id="password" placeholder="Mot de passe">
+            <button type="submit" class="btn btn-login">Se connecter</button>
+        </form>
+    </div>
+</div>
+
+<nav>
+    <h2>Menu</h2>
+    <a href="#">Accueil</a>
+    <a href="#">Liste des livres</a>
+</nav>
