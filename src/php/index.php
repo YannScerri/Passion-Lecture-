30c3b89a01339b1d2b9731f23359f897d9525a08
DanyCarn <pc23wsc@eduvaud.ch>
2024-11-19 10:41:42 +0100

    ajout de commentaires/en-têtes + liaison au fichier css

--- src/php/index.php
+++ src/php/index.php
@@ -1,11 +1,32 @@
+<?php 
+/**
+ * ETML
+ * Auteur : Dany Carneiro
+ * Date : 19.11.2024
+ * Description : Page d'acceuil de passion lecture comprenant une description du site et une liste des 5 derniers livres ajoutés
+ */
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
+    
 </head>
 <body>
-    
+    <!-- inclusion du header-->
+    <?php include("./header.php")?>
+
+
+
+
+    <!-- inclusion du footer-->
+    <?php include("./footer.php")?>
 </body>
 </html>
