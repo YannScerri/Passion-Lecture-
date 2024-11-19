da9ac840d49a8d6bb01602b98dffd3135b640bc4
DanyCarn <pc23wsc@eduvaud.ch>
2024-11-19 09:41:05 +0100

    suppression de la connexion dans le header et regroupement du header dans une div

--- src/php/header.php
+++ src/php/header.php
@@ -1,19 +1,10 @@
-<div class="container-header">
+<div class="header">
     <div class="titre-header">
         <h1>Passion Lecture</h1>
     </div>
-    <div class="login-container">
-        <form action="#" method="post">
-            <label for="user"> </label>
-            <input type="text" name="user" id="user" placeholder="Login">
-            <label for="password"> </label>
-            <input type="password" name="password" id="password" placeholder="Mot de passe">
-            <button type="submit" class="btn btn-login">Se connecter</button>
-        </form>
-    </div>
-</div>
 
-<nav>
-    <a href="#">Accueil</a>
-    <a href="#">Liste des livres</a>
-</nav>
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Liste des livres</a>
+    </nav>
+</div>
