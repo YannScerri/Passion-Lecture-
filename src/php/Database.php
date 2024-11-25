0b1a3aa1c2260a359c2b43ba6599af83c4f0f8a7
Maxime Louis Gabriel Pelloquin <pt11pfm@eduvaud.ch>
2024-11-25 08:21:48 +0100

    Nouvelle base de travail

--- src/php/Database.php
+++ src/php/Database.php
@@ -9,9 +9,76 @@
 
 class Database{
 
+    // Variable de classe
     private $connector;
 
-    
+    public function __construct()
+    {
+        try
+        {
+            // Lire le fichier de config json
+            $json = file_get_contents('../json/config.json');
+
+            // Charger la configuration depuis le fichier JSON
+            $config = json_decode($json, true);
+
+            // Construire la chaîne de connexion
+            $dsn = 'mysql:host=' . $config['database']['host'] . 
+                   ';port=' . $config['database']['port'] . 
+                   ';dbname=' . $config['database']['dbname'] . 
+                   ';charset=' . $config['database']['charset'];
+
+            // Se connecter à la base de données
+            $this->connector = new PDO($dsn, $config['database']['username'], $config['database']['password']);
+        }
+        catch (PDOException $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Methode permettant d'executer une simple requete sql
+     */
+    private function querySimpleExecute($query){
+
+        // Effectue une simple requete
+        $req = $this->connector->query($query);
+        return $req;
+    }
+
+    /**
+     * Methode permettant d'executer une simple requete sql en utilisant "prepare"
+     */
+    private function queryPrepareExecute($query, $binds = []){
+        $req = $this->connector->prepare($query);
+
+        foreach ($binds as $key => $value) {
+            // On utilise PDO::PARAM_STR par défaut, mais tu peux adapter en fonction des besoins
+            $req->bindValue(':' . $key, $value, is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR);
+    }
+        $req->execute();
+        return $req;
+    }
+
+    /**
+     * TODO: à compléter
+     */
+    private function formatData($req){
+
+        // traitement des données pour les retourner par exemple en tableau associatif (avec PDO::FETCH_ASSOC)
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    /**
+     * TODO: à compléter
+     */
+    private function unsetData($req){
+        // TODO: vider le jeu d’enregistrement
+        $req->closeCursor();
+    }
 }
 
 
