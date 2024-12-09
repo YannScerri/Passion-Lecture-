4e37b5e2edb02d741b9b0365e11f6d98e8fba608
DanyCarn <pc23wsc@eduvaud.ch>
2024-12-09 10:41:40 +0100

    modification de livre finie

--- src/php/modifyBookAction.php
+++ src/php/modifyBookAction.php
@@ -18,11 +18,19 @@
 
  $user = $_SESSION['user']['id'];
 
- if(!isset($_POST["bookCover"])){
-    $image = $book["image"];
- } else{
-    $image = $_POST["bookCover"];
- }
+ $image = $_FILES['bookCover'];
+
+ //si aucune image n'a été ajoutée dans le formulaire, l'image déjà dans la base de données est sélectionnée
+ //si une image est rentrée, supprime l'ancienne et stocke la nouvelle
+if (isset($_FILES['bookCover']) && $_FILES['bookCover']['error'] === 0) {
+    $dest = '../images/' . date("YmdHis");
+    $dest .= $image['name'];
+    move_uploaded_file($image['tmp_name'], $dest);
+    unlink($db->getBookImage($_POST['bookId']));
+} else {
+    $dest = $book["image"];
+}
+ 
 
  //si l'auteur rentré n'existe pas, le crée
  if(!$db->doesAuthorExists($_POST["firstName"], $_POST["lastName"])){
@@ -39,7 +47,7 @@
  $author = $db->getAuthorID($_POST["firstName"], $_POST["lastName"]);
 
  //modification du livre
- $db->modifyBook($_POST["bookId"], $_POST["title"], $_POST["bookExcerpt"], $_POST["bookSummary"], $year, $image, $_POST["pagesNumber"], $user, $_POST["category"], $editor, $author);
+ $db->modifyBook($_POST["bookId"], $_POST["title"], $_POST["bookExcerpt"], $_POST["bookSummary"], $year, $dest, $_POST["pagesNumber"], $user, $_POST["category"], $editor, $author);
 
  header('Location: ./index.php');
 ?>
