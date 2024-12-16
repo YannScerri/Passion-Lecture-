b884196604eea75bd0610b65aabcc43423f9cdf5
DanyCarn <pc23wsc@eduvaud.ch>
2024-12-16 09:14:02 +0100

    création du bouton de déconnexion et de son action

--- src/header.php
+++ src/header.php
@@ -13,6 +13,10 @@ session_start()
         </nav>
     </div>
 
+    <!-- affichage du bouton de déconnexion si l'utilisateur est connecté -->
+    <?php if($_SESSION['user']['isConnected']) : ?>
+        <button><a href="./Disconnect.php">Se déconnecter</a></button>
+    <?php endif; ?>
     <a href="./connection.php"  class="userLink"><img src="./images/user.png" alt="icône de connexion" class="userIcon"></a>
 
 </div>
